a17c2baa6f12ef30d5db1d17ce56dcb199383f9d
Sujeet Singh <sujeet@Sujeets-MacBook-Pro.local>
2022-06-13 22:32:16 +0545

    Initialize project and design landing page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
+import React from "react"
+import AOS from "aos";
+import "aos/dist/aos.css";
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
   return <Component {...pageProps} />
 }
 
